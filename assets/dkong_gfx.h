cebfcae53c210b48ee3c65a6b05d941a28be3284
Jean-François Fabre <jotd666@gmail.com>
2024-01-08 23:59:12 +0100

    first commit with Mark graphics & MAME commented Z80 file

---
+++ assets/dkong_gfx.h
@@ -0,0 +1,10 @@
+#ifndef __DKONG_GFX_H__
+#define __DKONG_GFX_H__
+
+#define NUM_COLOURS          256
+#define NUM_CLUTS            256
+
+#define NUM_TILES            256
+#define NUM_SPRITES          128
+
+#endif // __DKONG_GFX_H__
